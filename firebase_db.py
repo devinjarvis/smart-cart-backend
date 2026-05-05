7252e5ff9fbf463060447f63c587f8cf6a171484
Devin <devinsiju@gmail.com>
2026-05-05 21:28:24 +0530

    fix firebase env

--- firebase_db.py
+++ firebase_db.py
@@ -1,8 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os, json
 
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    if "FIREBASE_KEY" in os.environ:
+        cred_dict = json.loads(os.environ["FIREBASE_KEY"])
+        cred = credentials.Certificate(cred_dict)
+    else:
+        raise Exception("FIREBASE_KEY not set in environment variables")
+
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
